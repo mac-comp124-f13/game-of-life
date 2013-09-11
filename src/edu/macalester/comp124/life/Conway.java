ffb6b921d801457fc3e0937549c3e7528553362d
Shilad Sen <shilad@gmail.com>
2013-09-11 11:15:27 -0500

    Refined logic.

--- src/edu/macalester/comp124/life/Conway.java
+++ src/edu/macalester/comp124/life/Conway.java
@@ -2,23 +2,73 @@ package edu.macalester.comp124.life;
 
 /**
  * RuleSet implementing Conway's Game of Life.
- * @author Michael Ekstrand <ekstrand@cs.umn.edu>
  *
+ * @author Michael Ekstrand <ekstrand@cs.umn.edu>
  */
 public class Conway implements RuleSet {
-    
+
     public String getName() {
         return "Conway's Rules";
     }
 
     /**
      * Applies the rules of Conway's Game of Life.
-     * @param isAlive The value of the current cell (true = alive).
+     *
+     * @param isAlive       The value of the current cell (true = alive).
      * @param neighborCount The number of living neighbors of the cell.
      * @return true if the cell should be alive in the next generation.
-     * 
      */
     public boolean applyRules(boolean isAlive, int neighborCount) {
-        return false; // TODO: replace this with the correct rules
+        /*
+        if (isAlive && neighborCount == 2) {
+            return true;
+        } else if (isAlive && neighborCount == 3) {
+            return true;
+        } else if (!isAlive && neighborCount == 3) {
+            return true;
+        }
+        return false;
+        */
+
+        if (isAlive) {
+            return (neighborCount == 2 || neighborCount == 3) ;
+        } else {
+            return (neighborCount == 3);
+        }
+
+        /*
+        if (isAlive && neighborCount < 2) {
+            return false;
+        } else if (isAlive && (neighborCount == 2 || neighborCount == 3)) {
+            return true;
+        } else if (isAlive && neighborCount > 3) {
+            return false;
+        } else if (!isAlive && neighborCount == 3) {
+            return true;
+        } else {
+            return isAlive;
+        }
+        */
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
